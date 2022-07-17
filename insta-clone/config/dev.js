40596cecbc383204ffe495ba0bcf9a1391fcd8f8
Muskan Mishra <muskanbhargav05@gmail.com>
2022-07-17 16:43:34 +0530

    change

--- insta-clone/config/dev.js
+++ insta-clone/config/dev.js
@@ -1,7 +1,7 @@
 const { SENDGRID_API } = require("./prod");
 
 module.exports={
-    MONGOURI : "mongodb+srv://muskan05:LE65dKhShz9EDXIS@cluster0.cb62f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    MONGOURI:"mongodb+srv://muskan05:LE65dKhShz9EDXIS@cluster0.cb62f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     JWT_SECRET:"effertlove",
     SENDGRID_API:"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",
     EMAIL:"vaibhavchandra2908@gmail.com"
